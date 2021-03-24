7e9ddddabfd36f37694cf7536cdd47e9c4526c5c
alex-ismailov <alexx.ismailov@gmail.com>
2021-03-24 15:12:17 +0300

    [#187] remove empty string

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ const userName = 'Alex Smile';
 console.log(`Hello ${userName}`);
 /* ************************************ */
 
-
 /* *** Этот код внесен by alex-ismailov *** */
 const userName2 = 'Alex2 Smile2';
 console.log(`Hello2 ${userName2}`);
