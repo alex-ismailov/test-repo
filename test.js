9d06d37070723ef6383df7df879d231e90e6c367
alex-ismailov <alexx.ismailov@gmail.com>
2021-11-21 23:42:13 +0300

    Add commit 1 from mac

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-/* *** Этот код внесен by Alex-Smil *** */
-const userName = 'Alex Smile';
-console.log(`Hello ${userName}`);
-/* ************************************ */
-
-/* *** Этот код внесен by alex-ismailov *** */
-const userName2 = 'Alex2 Smile2';
-console.log(`Hello2 ${userName2}`);
-/* ************************************ */
-
-/* *** Этот код внесен by alex-ismailov *** */
-const userName3 = 'Alex2 Smile3';
-console.log(`Hello3 ${userName3}`);
-/* ************************************ */
+// commit 1 from mac
