8dad9f6470002abbe5921486c7a78c70d4610d18
alex-ismailov <alexx.ismailov@gmail.com>
2022-02-10 01:07:51 +0300

    Add commit 3, 4, 5 from mac in main branch after git reset --soft

--- grb-tests.js
+++ grb-tests.js
@@ -1,3 +1,5 @@
 // commit 1 from mac in grb-tests branch
 // commit 2 from mac in grb-tests branch
 // commit 3 from mac in main branch
+// commit 4 from mac in main branch
+// commit 5 from mac in main branch
